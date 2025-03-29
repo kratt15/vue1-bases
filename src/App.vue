7bc234bcab7a595f0aa4aef9533405b56f49e108
kratt15 <sidneyadjoh15@gmail.com>
2025-03-29 00:41:36 +0000

    Modification de l'application pour mettre à jour le message d'accueil et introduire une fonctionnalité de calcul du nom complet à partir des prénoms et noms. Suppression de la gestion des éléments de liste.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,15 @@
 <script setup>
-  import { ref } from 'vue'
+  import { ref, computed } from 'vue'
     let id = 0
   //variable
-  const msg = ref('Tuto vue.js  ')
-  const newItem = ref('')
-  const items = ref([
-    { id: id++, name: 'item 1' },
-    { id: id++, name: 'item 2' },
-    { id: id++, name: 'item 3' },
-    { id: id++, name: 'item 4' },
-    { id: id++, name: 'item 5' },
-  ])
-  // function
-  const addItem = () => {
-    items.value.push({ id: id++, name: newItem.value })
-    newItem.value = ''
-  }
-  const deleteItem = (id) => {
-    items.value = items.value.filter(item => item.id !== id)
-  }
+  const msg = ref('Tuto vue.js 2025')
+  const fName = ref('Sidney')
+  const lName = ref('Consti')
+
+  const fullname = computed(() => {
+    return fName.value + " " + lName.value.toUpperCase()
+  })
+ 
 
 </script>
 
@@ -27,15 +18,7 @@
 
   <main id="main">
         <h1 >{{ msg }}</h1>
-       <ul>
-        <li v-for="item in items" :key="item.id">
-          {{ item.name }}
-          <button @click="deleteItem(item.id)">X</button>
-        </li>
-     
-       </ul>
-       <input type="text" v-model="newItem">
-       <button @click="addItem">Ajouter</button>
+        <h2>Bonjour  {{ fullname }}</h2>
     
   </main>
 </template>
